707fae863bf98d02588ab87d38882159ba090105
Mark <voronkov1mark@gmail.com>
2023-05-01 23:47:26 +0300

    all in controller

--- src/main/java/pro/sky/java/course2/skyprospring/CalculatorHello.java
+++ src/main/java/pro/sky/java/course2/skyprospring/CalculatorHello.java
@@ -1,8 +1,43 @@
 package pro.sky.java.course2.skyprospring;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorHello {
+    @GetMapping(path = "/calculator")
+    public String hello() {
+        return "<b>Добро пожаловать в калькулятор!</b>";
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String plus(@RequestParam int num1, int num2) {
+        int total = num1 + num2;
+        return Integer.toString(total);
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String minus(@RequestParam int num1, int num2) {
+        int total = num1 - num2;
+        return Integer.toString(total);
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String multiply(@RequestParam int num1, int num2) {
+        int sum = num1 * num2;
+        return Integer.toString(sum);
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String divide(@RequestParam int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return "На ноль делить нельзя!";
+        } else {
+            int sum = num1 / num2;
+            return Integer.toString(sum);
+        }
+    }
+
 }
