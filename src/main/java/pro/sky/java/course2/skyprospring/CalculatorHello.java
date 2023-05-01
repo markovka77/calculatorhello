812554c7b625ad74439afdb84dd5b36b65b5fbb1
Mark <voronkov1mark@gmail.com>
2023-05-01 22:44:04 +0300

    initial commit

---
+++ src/main/java/pro/sky/java/course2/skyprospring/CalculatorHello.java
@@ -0,0 +1,8 @@
+package pro.sky.java.course2.skyprospring;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorHello {
+}
---
+++ src/main/java/pro/sky/java/course2/skyprospring/SkyproSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.skyprospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringApplication.class, args);
+	}
+
+}
