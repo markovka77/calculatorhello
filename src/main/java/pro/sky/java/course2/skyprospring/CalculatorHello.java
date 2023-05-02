7d2b5547ccc0738e5763c2f56c143b6be35397dd
Mark <voronkov1mark@gmail.com>
2023-05-02 11:33:26 +0300

    done

--- src/main/java/pro/sky/java/course2/skyprospring/CalculatorHello.java
+++ src/main/java/pro/sky/java/course2/skyprospring/CalculatorHello.java
@@ -9,39 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorHello {
     @GetMapping(path = "/calculator")
     public String hello() {
-        return "<b>Добро пожаловать в калькулятор!</b>";
+        return ServiceCalculator.hello();
     }
 
     @GetMapping(path = "/calculator/plus")
     public String plus(@RequestParam int num1, int num2) {
-        int total = num1 + num2;
-        String sum = Integer.toString(num1)+" + "+ Integer.toString(num2)+ " = "+Integer.toString(total);
-        return sum;
+               return ServiceCalculator.plus(num1,num2);
     }
 
     @GetMapping(path = "/calculator/minus")
     public String minus(@RequestParam int num1, int num2) {
-        int total = num1 - num2;
-        String sum = Integer.toString(num1)+" - "+ Integer.toString(num2)+ " = "+Integer.toString(total);
-        return sum;
+       return ServiceCalculator.minus(num1,num2);
     }
 
     @GetMapping(path = "/calculator/multiply")
     public String multiply(@RequestParam int num1, int num2) {
-        int total = num1 * num2;
-        String sum = Integer.toString(num1)+" * "+ Integer.toString(num2)+ " = "+Integer.toString(total);
-        return sum;
+        return ServiceCalculator.multiply(num1, num2);
     }
 
     @GetMapping(path = "/calculator/divide")
     public String divide(@RequestParam int num1, int num2) {
-        if (num1 == 0 || num2 == 0) {
-            return "На ноль делить нельзя!";
-        } else {
-            int total = num1 / num2;
-            String sum = Integer.toString(num1)+" + "+ Integer.toString(num2)+ " = "+Integer.toString(total);
-            return sum;
+        return ServiceCalculator.divide(num1, num2);
         }
     }
 
-}
+
